dbe25453580f55e676f6081eb8993bc8322d7958
Niko-MNW <47422338+NikoMNW@users.noreply.github.com>
2020-12-09 16:12:12 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -65,14 +65,14 @@ rm /usr/bin/axe*
 
 #Installing Daemon
  cd ~
-wget https://github.com/AXErunners/axe/releases/download/v1.4.1/axecore-1.4.1-x86_64-linux-gnu.tar.gz
-tar -xzf axecore-1.4.1-x86_64-linux-gnu.tar.gz -C ~/AXE-MN-setup
-rm -rf axecore-1.4.1-x86_64-linux-gnu.tar.gz
+wget https://github.com/AXErunners/axe/releases/download/v1.6.1.1/axecore-1.6.1.1-x86_64-linux-gnu.tar.gz
+tar -xzf axecore-1.6.1.1-x86_64-linux-gnu.tar.gz -C ~/AXE-MN-setup
+rm -rf axecore-1.6.1.1-x86_64-linux-gnu.tar.gz
  
  # Deploy binaries to /usr/bin
- sudo rm ~/AXE-MN-setup/axecore-1.4.1/bin/axe-qt
- sudo rm ~/AXE-MN-setup/axecore-1.4.1/bin/test*
- sudo cp ~/AXE-MN-setup/axecore-1.4.1/bin/axe* /usr/bin/
+ sudo rm ~/AXE-MN-setup/axecore-1.6.1.1/bin/axe-qt
+ sudo rm ~/AXE-MN-setup/axecore-1.6.1.1/bin/test*
+ sudo cp ~/AXE-MN-setup/axecore-1.6.1.1/bin/axe* /usr/bin/
  sudo chmod 755 -R ~/AXE-MN-setup
  sudo chmod 755 /usr/bin/axe*
  
